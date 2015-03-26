d00f4bab704e68e74f49bd3250058aa3e620f22e
japgunbanga <japgunbanga@hotmail.com>
2015-03-26 17:15:34 -0400

    Changes were made to the previous post

--- single-CustomPostType.php
+++ single-CustomPostType.php
@@ -1,26 +1,31 @@
 <?php
  /*Template Name: Cake Template  
  */
-  
+  //This line of code adds styling to this plugin from the style sheet
 get_header(); ?>
+<style>
+<?php include 'style.css'; ?>
+</style>
+
 <div id="primary">
     <div id="content" role="main">
     <?php
     $mypost = array( 'post_type' => 'cake', );
-	// retrives all the posts from database	
+	// This line of code retrives all the posts from database	
     $loop = new WP_Query( 'post_type=cake' );
     ?>
     <?php while ( $loop->have_posts() ) : $loop->the_post();?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="position:relative;width:100%;">
             <header class="entry-header">
  
-                <!-- Display featured image in right-aligned floating div -->
+                <!-- This line of code displays featured image in right-aligned floating div -->
 				<br /> <br />
                 <div style="float: right; margin: 10px">
                     <?php the_post_thumbnail( array( 100, 100 ) ); ?>
                 </div>
  
-                <!-- Display Cake Flavor, Author Name and Recipe -->
+                <!-- This line of code prints Cake Flavor, Author Name and Recipe -->
+				<section class="info">
                 <strong>Cake: </strong> <?php echo esc_html( get_post_meta( get_the_ID(), '_cake_flavor', true ) ); ?><br />
                 <strong>Author: </strong>
                 <?php echo esc_html( get_post_meta( get_the_ID(), '_cake_author', true ) ); ?>
@@ -29,10 +34,11 @@ get_header(); ?>
                 <?php
                 echo get_post_meta( get_the_ID(), '_cake_recipe', true );
                 ?>
+				</section>
             </header>
  
 
-        </article>
+        </article> <br/>
  
     <?php endwhile; ?>
     </div>
